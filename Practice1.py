ce1ab9f6f867f507f9dcf20054d8db29a583aecd
aashish <aashishthakor496@email.com>
2025-11-09 17:39:52 +0530

    String In Python

--- Practice1.py
+++ Practice1.py
@@ -1,12 +1,3 @@
-import os
-
-# Specify the path (use '.' for current directory)
-path = '.'
-
-# Get the list of files and folders
-contents = os.listdir(path)
-
-# Print the contents
-print("Contents of the directory:")
-for item in contents:
-    print(item)
+a=12
+b=34
+print("The Sum Of Two Numbers is:",a%b)
--- User Input.py
+++ User Input.py
@@ -1,4 +1,3 @@
-a=(input("Enter first number: "))
-b=(input("Enter second number: "))
-
+a=12
+b=20
 print("The Sum Of Two Numbers is:",a+b)
