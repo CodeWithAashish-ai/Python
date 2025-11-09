f5f2d975028d0c159b4e6b4ed33f3ec871bfcf7e
aashish <aashishthakor496@email.com>
2025-11-09 14:41:34 +0530

    User Input

--- Operator.py
+++ Operator.py
@@ -1,6 +1,44 @@
-a = 10
-b = 20
+# Arithmetical operators
+a=30
+b=15
+print("a + b =", a + b)  # Addition
+print("a - b =", a - b)  # Subtraction
+print("a * b =", a * b)  # Multiplication
+print("a / b =", a / b)  # Division
+print("a % b =", a % b)  # Modulus
 
-print(a > 5 and b > 15)   # True  (dono true hain)
-print(a > 15 or b > 15)   # True  (ek true hai)
-print(not(a > 5))         # False (ulta kar diya)
+
+# assignment operators
+a=40
+b=12
+a=b
+print(a)
+a+=b
+print(a)
+a-=b
+print(a)
+a*=b
+print(a)
+a/=b
+print(a)
+a%=b
+print(a)
+
+# comparison operators
+a=25
+b=20
+
+print("a == b:", a == b)  # Equal to
+print("a != b:", a != b)  # Not equal to
+print("a > b:", a > b)    # Greater than
+print("a < b:", a < b)    # Less than
+print("a >= b:", a >= b)  # Greater than or equal to
+print("a <= b:", a <= b)  # Less than or equal to
+
+# logical operators
+a=30
+b=25
+c=10
+print(a>b and a>c)
+print(a>b or a<c)
+print(not(a>b and a>c))
---
+++ User Input.py
@@ -0,0 +1,4 @@
+a=(input("Enter first number: "))
+b=(input("Enter second number: "))
+
+print("The Sum Of Two Numbers is:",a+b)
