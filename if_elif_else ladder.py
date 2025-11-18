3e30886eb972a40d291823be5131f4d742b7e263
aashish <aashishthakor496@email.com>
2025-11-18 07:42:10 +0530

    If_Elif_Else Ladder In Python

---
+++ if_elif_else ladder.py
@@ -0,0 +1,13 @@
+a = int(input("Enter The Value:"))
+
+if (a >= 18):
+    print("Eligible for voting")
+
+elif (a <0):
+    print("Young people can vote")
+
+elif (a==0):
+    print("Not eligible for voting")
+
+else:
+    print("Not eligible for voting")
